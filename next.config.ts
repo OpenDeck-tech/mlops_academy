91874c44564b8afeee44c6097709d650c6c8b064
Tobi John <tobioluwafemi58@gmail.com>
2025-10-28 11:07:04 +0000

    Fix turbopack root directory warning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      root: "/Users/tobijohn/mlops_learning/mlops-academy",
+    },
+  },
 };
 
 export default nextConfig;
