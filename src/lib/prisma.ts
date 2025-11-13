1e71b1905b36914306841c43d5eac359e59733f8
Tobi John <tobioluwafemi58@gmail.com>
2025-11-13 12:02:26 +0000

    Adds Prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from 'app/generated-prisma-client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+export default prisma
+
