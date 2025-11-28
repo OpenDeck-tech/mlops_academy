3450e7a7872daab6ce8b2c9d3046d933b0c86bdd
Tobi John <tobioluwafemi58@gmail.com>
2025-11-28 22:24:20 +0000

    updates next config

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ import path from "path";
 const nextConfig: NextConfig = {
   // Use process.cwd() to get the current working directory dynamically
   outputFileTracingRoot: path.join(process.cwd()),
-  eslint: {
-    // Don't fail build on ESLint warnings (only errors)
-    ignoreDuringBuilds: false,
-  },
   typescript: {
     // Don't fail build on TypeScript errors (ESLint will catch them)
     ignoreBuildErrors: false,
